bba2e82fca8bcdf40109f41ac4d669eae15dc71c
SHIVAM15SEP <shivamroy80116@gmail.com>
2025-10-23 12:50:19 +0530

    add  maths_operation.py

---
+++ maths_operation.py
@@ -0,0 +1,18 @@
+# Perfom basic mathematical operation
+num1 = float(input("Enter the first number:"))
+num2 = float(input("Enter the second number:"))
+#Addition
+addition = num1 + num2
+#Subtraction
+subtraction = num1 - num2
+#Multiplication
+multiplication = num1 * num2
+#Division
+division = num1 / num2
+
+# Display the result
+print("Results of mathematical operations")
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
